66c73bcaa3dc3fbf0672c24b1a845b8d540fdb47
Porter Libby <pelibby16@earlham.edu>
2023-04-17 23:33:55 -0400

    update

--- ponderbot.py
+++ ponderbot.py
@@ -1,3 +1,16 @@
+#     _______                            __                      __                    __     
+#    /       \                          /  |                    /  |                  /  |    
+#    $$$$$$$  | ______   _______    ____$$ |  ______    ______  $$ |____    ______   _$$ |_   
+#    $$ |__$$ |/      \ /       \  /    $$ | /      \  /      \ $$      \  /      \ / $$   |  
+#    $$    $$//$$$$$$  |$$$$$$$  |/$$$$$$$ |/$$$$$$  |/$$$$$$  |$$$$$$$  |/$$$$$$  |$$$$$$/   
+#    $$$$$$$/ $$ |  $$ |$$ |  $$ |$$ |  $$ |$$    $$ |$$ |  $$/ $$ |  $$ |$$ |  $$ |  $$ | __ 
+#    $$ |     $$ \__$$ |$$ |  $$ |$$ \__$$ |$$$$$$$$/ $$ |      $$ |__$$ |$$ \__$$ |  $$ |/  |
+#    $$ |     $$    $$/ $$ |  $$ |$$    $$ |$$       |$$ |      $$    $$/ $$    $$/   $$  $$/ 
+#    $$/       $$$$$$/  $$/   $$/  $$$$$$$/  $$$$$$$/ $$/       $$$$$$$/   $$$$$$/     $$$$/  
+#                                                                                         
+#                                             v1.0   /   April 2023   /   Porter Libby                                                                              
+#                                                                                         
+################# Imports #################
 import json
 from fuzzywuzzy import fuzz
 import os
@@ -5,20 +18,26 @@ import discord
 from dotenv import load_dotenv
 import re
 
-# bot.py
+################# BOT SETUP #################
+# Set up bot
 load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client(command_prefix='$', intents=discord.Intents.all())
 
-# Load the JSON file
+# Load card data
 with open('unique-cards.json') as f:
     data = json.load(f)
 
+################# FUZZY SEARCH #################
 def search(input_str):
     # Find the closest match to the input string
-    max_score = 0
+    max_score = 0 # highest probability of a matching term
     closest_match = None
     for item in data:
         name = item['name']
-        score = fuzz.ratio(name.lower(), input_str.lower())
+        score = fuzz.ratio(name.lower(), input_str.lower()) # calculate probability
+
+        # if probability good, set as new best choice
         if score > max_score:
             max_score = score
             closest_match = item
@@ -26,43 +45,39 @@ def search(input_str):
     # Print the closest match
     if closest_match:
         if max_score > int(os.getenv('CONFIDENCE_THRESHOLD')):
+            # if match is good enough to return to use
             return ("Search: " + input_str + " (" + closest_match['name'] + ")", closest_match['image_uris']['large'], "Confidence: " + str(max_score) + "%")
         else:
+            # if match is not good enough
             return ("Search: " + input_str, None, "Uncertain")
 
-    else:
-        print("No match found. ")
-
-
-TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client(command_prefix='$', intents=discord.Intents.all())
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
+############# DISCORD MESSAGE EVENT #############
 @client.event
 async def on_message(message):
-    print(message.content)
     if message.author.bot == False:
+        # Search message for {terms}
         searchls = re.findall('{.*?}', message.content)
+
+        # If there are {terms}
         if (searchls != []):
             url_list = []
             name_list = []
             score_list = []
 
+            # for each term
             for searchterm in searchls:
-                output = search(searchterm.replace("}","").replace("{",""))
-                
+                output = search(searchterm.replace("}","").replace("{","")) # search for matching cards
                 name_list.append(output[0])
                 if output[1]: url_list.append(output[1])
                 score_list.append(output[2])
 
+            # Create output string
             outstr = ""
             if len(url_list) > 0: outstr = "\n".join(url_list) + "\n"
-            for x in range(len(name_list)):
-                outstr += name_list[x] + " (" + score_list[x] + "). \n"
-            
+            for x in range(len(name_list)): outstr += name_list[x] + " (" + score_list[x] + "). \n"
+
+            # Send message reply to user
             await message.channel.send(outstr, reference=message)
             
-
+################# START BOT #################
 client.run(TOKEN)
